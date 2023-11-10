9bacdb82f50f894c6134b261122a1037eb060fdd
Sandeep Kumawat <96002657+Sandeep2042001@users.noreply.github.com>
2023-11-10 09:32:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+// Badu Pride 10th October 2019 https://github.com/sandeep2042001/
+var config = {
+    // The length of the sentences can be arbitrary. You can write ten or twenty sentences.
+    // Try not to exceed 15 characters per sentence, as the display effect may not be good.
+    texts: [
+        "Happy Birthday again",
+        "You are so Beautifull",
+        "Tu Hamesha Hasti rhe",
+        "Apni Dosti Kabhi naa tute",
+        "Today is your birthday",
+        "Teri aankho main kabhi aansu naa aaye",
+        "Tu Khuub moti hoo jaaye",
+        "Or tujhe roj khaane ko momos mil jaaye",
+        "I Hop ab tu pdne pr bhi dhyaan degi",
+    ],
+    /**
+     * imgs can be left blank, but if you want to fill it in, it must follow the format below
+     * "Corresponding to the above text, must be exactly the same": "Image address, you can put the image in the imgs folder"
+     * For example,
+     * "My beloved sweetheart": "./imgs/xiaokeai.jpg"
+     *
+     * If you don't want an image, just start each line with two slashes, as in the "Today is your birthday" example below:)
+     * Tip: Images are best in square or near-square, it looks better.
+     */
+    imgs: {
+        "You are so Beautifull": "./imgs/akansha.png",
+    },
+    // Button text description, the following are the default English button text, you can change it to what you like
+    desc: {
+        turn_on: "Thodi lighting hoo Jaaye ?",
+        play: "Aapka fav song?",
+        bannar_coming: "Now",
+        balloons_flying: "Seems to be missing something",
+        cake_fadein: "Cake?",
+        light_candle: "Candle?",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+        
+    }
+};
